776bb3bf527512581ec499a93be968656e46d8b5
PabloAndresZP <pablozeladaparedes@gmail.com>
2023-09-04 06:48:54 -0400

    Resolución de conflictos

---
+++ lib/src/pages/game2_eje3_page.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:mi_app_imgsound/src/widgets/coin_counter.dart';
+import 'package:mi_app_imgsound/src/widgets/hearts_indicator.dart';
+
+class Game2Eje3Page extends StatefulWidget {
+  final int initialHearts;
+
+  Game2Eje3Page({required this.initialHearts});
+
+  @override
+  _Game2Eje3PageState createState() => _Game2Eje3PageState();
+}
+
+class _Game2Eje3PageState extends State<Game2Eje3Page> {
+  late AudioPlayer _audioPlayer;
+  int selectedAnswer = -1;
+  int coins = 0;
+  late int hearts;
+  int correctAnswer = 1;  // Suponiendo que la segunda imagen es la correcta
+
+  List<String> exerciseImages = [
+    'assets/images/bosquet.png',
+    'assets/images/personam.png',
+  ];
+
+  List<String> exerciseAudios = [
+    'assets/sounds/escmayor.mp3',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+    hearts = widget.initialHearts;
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  void _playPause(String audioUrl) async {
+    int result = await _audioPlayer.play(audioUrl);
+    if (result != 1) {
+      print("Error al reproducir el audio");
+    }
+  }
+
+  void _checkAnswer() {
+    if (selectedAnswer == correctAnswer) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('¡Bien!'),
+        duration: Duration(seconds: 3),
+      ));
+
+      setState(() {
+        coins += 10; 
+      });
+
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Incorrecto'),
+        duration: Duration(seconds: 2),
+      ));
+
+      setState(() {
+        hearts -= 1;
+      });
+
+      if (hearts <= 0) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Juego Terminado'),
+            content: Text('Has perdido todos tus corazones. ¿Quieres intentarlo de nuevo?'),
+            actions: [
+              TextButton(
+                child: Text('Sí'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Image.asset('assets/images/icon_atras.png'),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: Text(
+          'Pintando sonido',
+          style: TextStyle(
+            color: Color(0xFFFDFDFD),
+            fontFamily: 'WorkSans',
+            fontWeight: FontWeight.bold,
+            fontSize: 21.5,
+          ),
+        ),
+        backgroundColor: Color(0xFF030328),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/textura_2.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CoinCounter(coins),
+                    HeartsIndicator(hearts),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  'Selecciona la imagen que se asocia con el audio',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFFFDFDFD),
+                    fontFamily: 'WorkSans',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 21.5,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: exerciseImages.map((imagePath) {
+                    int index = exerciseImages.indexOf(imagePath);
+                    return Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedAnswer = index;
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: (selectedAnswer == index)
+                                  ? Colors.blue
+                                  : Colors.transparent,
+                              width: 3.0,
+                            ),
+                          ),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Image.asset(imagePath, fit: BoxFit.cover),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                ElevatedButton(
+                  onPressed: () => _playPause(exerciseAudios[0]),
+                  child: Icon(Icons.play_arrow, color: Colors.white),
+                ),
+                ElevatedButton(
+                  onPressed: _checkAnswer,
+                  child: Text('Verificar'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/pages/game2_page.dart
+++ lib/src/pages/game2_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:mi_app_imgsound/src/widgets/coin_counter.dart';
 import 'package:mi_app_imgsound/src/widgets/hearts_indicator.dart';
+import 'package:mi_app_imgsound/src/pages/game2_eje3_page.dart';
 
 class Game2Page extends StatefulWidget {
   @override
@@ -17,6 +18,26 @@ class _Game2PageState extends State<Game2Page> {
   int coins = 0;
   int hearts = 3;
 
+   List<int> correctAnswers = [1, 0]; 
+
+  int currentExerciseIndex = 0;
+  List<String> exerciseImages = [
+    'assets/images/bosquet.png',
+    'assets/images/personam.png',
+    // ... otras imágenes ...
+  ];
+  List<String> exerciseAudios1 = [
+    'assets/sounds/escmayor.mp3',
+    'assets/sounds/esccromatica.mp3',
+    
+    // ... otros audios ...
+  ];
+  List<String> exerciseAudios2 = [
+    'assets/sounds/escmenor.mp3',
+    'assets/sounds/modofrigio.mp3',
+    // ... otros audios ...
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -31,13 +52,104 @@ class _Game2PageState extends State<Game2Page> {
     super.dispose();
   }
 
-  void _playPause(String audioUrl, int playerNumber) {
-    // Aquí va tu código para manejar la reproducción de audio.
+  void _playPause(String audioUrl, int playerNumber) async {
+  AudioPlayer currentPlayer = (playerNumber == 1) ? _audioPlayer1 : _audioPlayer2;
+  AudioPlayer otherPlayer = (playerNumber == 1) ? _audioPlayer2 : _audioPlayer1;
+  bool currentPlaying = (playerNumber == 1) ? isPlaying1 : isPlaying2;
+
+  if (currentPlaying) {
+    await currentPlayer.pause();
+  } else {
+    await otherPlayer.stop(); // Detener el otro audio.
+    int result = await currentPlayer.play(audioUrl); 
+    if (result != 1) {
+      print("Error al reproducir el audio");
+    }
   }
 
-  void _checkAnswer() {
-    // Aquí va tu lógica de verificar respuestas.
+  setState(() {
+    if (playerNumber == 1) {
+      isPlaying1 = !isPlaying1;
+      isPlaying2 = false;
+    } else {
+      isPlaying2 = !isPlaying2;
+      isPlaying1 = false;
+    }
+  });
+}
+
+  void _goToNextExercise() {
+    if (currentExerciseIndex < exerciseImages.length - 1) {
+      setState(() {
+        currentExerciseIndex += 1;
+        selectedAnswer = -1;
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('¡Has completado todos los ejercicios!'),
+        duration: Duration(seconds: 2),
+      ));
+    }
   }
+
+void _checkAnswer() {
+  if (selectedAnswer == correctAnswers[currentExerciseIndex]) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('¡Bien!'),
+      duration: Duration(seconds: 3),
+    ));
+
+    setState(() {
+      completedExercises += 1;
+      coins += 10; 
+    });
+
+    if (completedExercises == correctAnswers.length) { 
+      Navigator.push(
+  context,
+  MaterialPageRoute(builder: (context) => Game2Eje3Page(initialHearts: hearts)),
+);
+
+    } else {
+      _goToNextExercise();
+    }
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('Incorrecto'),
+      duration: Duration(seconds: 2),
+    ));
+
+    setState(() {
+      hearts -= 1;
+    });
+
+    if (hearts <= 0) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Juego Terminado'),
+          content: Text('Has perdido todos tus corazones. ¿Quieres intentarlo de nuevo?'),
+          actions: [
+            TextButton(
+              child: Text('Sí'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Aquí puedes resetear cualquier variable si es necesario para reiniciar el juego.
+              },
+            ),
+            TextButton(
+              child: Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Puedes hacer cualquier otra acción necesaria si el jugador decide no intentarlo de nuevo.
+              },
+            ),
+          ],
+        ),
+      );
+    }
+  }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -101,7 +213,7 @@ class _Game2PageState extends State<Game2Page> {
                     fontSize: 21.5,
                   ),
                 ),
-                Image.asset('assets/images/bosquet.png'),
+                Image.asset(exerciseImages[currentExerciseIndex]),
                 SizedBox(height: 20.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -109,7 +221,7 @@ class _Game2PageState extends State<Game2Page> {
                     Column(
                       children: [
                         ElevatedButton(
-                          onPressed: () => _playPause('assets/audios/escmayor.mp3', 1),
+                          onPressed: () => _playPause(exerciseAudios1[currentExerciseIndex], 1),
                           child: Icon(isPlaying1 ? Icons.pause : Icons.play_arrow, color: Colors.white),
                           style: ButtonStyle(
                             backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF00D8BB)),
@@ -124,21 +236,12 @@ class _Game2PageState extends State<Game2Page> {
                             });
                           },
                         ),
-                        Text(
-                          'Escala Mayor',
-                          style: TextStyle(
-                            color: Color(0xFFFDFDFD),
-                            fontFamily: 'WorkSans',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 21.5,
-                          ),
-                        ),
                       ],
                     ),
                     Column(
                       children: [
                         ElevatedButton(
-                          onPressed: () => _playPause('assets/audios/escmenor.mp3', 2),
+                          onPressed: () => _playPause(exerciseAudios2[currentExerciseIndex], 2),
                           child: Icon(isPlaying2 ? Icons.pause : Icons.play_arrow, color: Colors.white),
                           style: ButtonStyle(
                             backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF00D8BB)),
@@ -153,34 +256,13 @@ class _Game2PageState extends State<Game2Page> {
                             });
                           },
                         ),
-                        Text(
-                          'Escala Menor',
-                          style: TextStyle(
-                            color: Color(0xFFFDFDFD),
-                            fontFamily: 'WorkSans',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 21.5,
-                          ),
-                        ),
                       ],
                     ),
                   ],
                 ),
-                SizedBox(height: 20.0),
                 ElevatedButton(
                   onPressed: _checkAnswer,
-                  child: Text(
-                    'Comprobar',
-                    style: TextStyle(
-                      color: Color(0xFFFDFDFD),
-                      fontFamily: 'WorkSans',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 21.5,
-                    ),
-                  ),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF00D8BB)),
-                  ),
+                  child: Text('Verificar'),
                 ),
               ],
             ),
